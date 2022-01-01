70899bf9e18c356a36979ef74103bf7e3de1118c
Etienne Schmidt <gladion136@googlemail.com>
2022-01-01 13:33:01 +0100

    Rework about activity * Fix texts * add menu

--- Android_Client/OpenSyncedLists/app/src/main/java/eu/schmidt/systems/opensyncedlists/activities/AboutActivity.java
+++ Android_Client/OpenSyncedLists/app/src/main/java/eu/schmidt/systems/opensyncedlists/activities/AboutActivity.java
@@ -16,11 +16,22 @@
  */
 package eu.schmidt.systems.opensyncedlists.activities;
 
+import static eu.schmidt.systems.opensyncedlists.utils.Constant.LOG_TITLE_DEFAULT;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import eu.schmidt.systems.opensyncedlists.BuildConfig;
@@ -54,5 +65,76 @@ public class AboutActivity extends AppCompatActivity
         
         TextView tvVersion = findViewById(R.id.tVVersion);
         tvVersion.setText("Version: " + BuildConfig.VERSION_NAME);
+    }
+    
+    /**
+     * In onCreateOptionsMenu the menu from the ActionBar is inflated.
+     *
+     * @param menu Menu to inflate
+     * @return true
+     */
+    @Override public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.about_menu, menu);
+        return true;
+    }
+    
+    /**
+     * onOptionsItemSelected handles the events from the ActionBar.
+     *
+     * @param item selected item
+     * @return action handled?
+     */
+    @Override public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                // Back to ListsActivity
+                onBackPressed();
+                return true;
+            case R.id.openInPlayStore:
+                try
+                {
+                    Intent rateIntent = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(String.format("%s?id=%s", "market://",
+                            getPackageName())));
+                    startActivity(rateIntent);
+                }
+                catch (ActivityNotFoundException e)
+                {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(
+                        "http://play.google.com/store/apps/details?id="
+                            + getPackageName())));
+                }
+                return true;
+            case R.id.contact_developer:
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL,
+                    new String[]{getString(R.string.dev_mail_address)});
+                intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.dev_mail_subject));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }else {
+                    Log.e(LOG_TITLE_DEFAULT,
+                        "No activity found to send mail");
+                    Toast.makeText(this,
+                        getString(R.string.no_app_for_intent_installed),
+                        Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case R.id.openWebpage:
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(getString(R.string.app_webpage))));
+                return true;
+            case R.id.showSourceCode:
+                startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(getString(R.string.sourcecode_webpage))));
+                return true;
+        }
+        // Item not handled.. pass to super
+        return super.onOptionsItemSelected(item);
     }
 }
